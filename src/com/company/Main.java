ff615c98ba913f26b74ff70f76bf60da75491594
Raccoon <o.sillycoon@gmail.com>
2020-12-21 10:39:15 +0300

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Staff[] staffArray = new Staff[5];
+	staffArray[0] = new Staff("Zanin Dmitry", "System Administrator", "sdmitry@company.com", "89267389274", 90000, 25);
+    staffArray[1] = new Staff("Chubchev Ivan", "Product Manager", "civan@company.com", "89638492709", 60000, 50);
+    staffArray[2] = new Staff("Zemskaya Yana", "Office Administrator", "zyana@company.com", "89178407358", 55000, 39);
+    staffArray[3] = new Staff("Lantrat Olga", "Web Designer", "lolga@company.com", "89479284976", 110000, 45);
+    staffArray[4] = new Staff("Pridanov Valery", "Engineer", "pvalery@company.com", "89178407394", 70000, 55);
+
+        for (int i = 0; i < staffArray.length; i++) {
+            if(staffArray[i].getAge() > 40) {
+                staffArray[i].printInfo();
+            }
+        }
+    }
+}
---
+++ src/com/company/Staff.java
@@ -0,0 +1,76 @@
+package com.company;
+
+public class Staff {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    Staff(String fullName, String position, String email, String phone, int salary, int age){
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void printInfo(){
+        System.out.println("ФИО: " + fullName +
+                "\nДолжность: " + position +
+                "\nE-Mail: " + email +
+                "\nТелефон: " + phone +
+                "\nЗарплата: " + salary +
+                "\nВозраст: " + age + "\n");
+    }
+}
